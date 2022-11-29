083e1d00860ab4d346f1c6dd76e8fc0bd112a3c7
Raghav  Agrawal <66827401+simply-raghav@users.noreply.github.com>
2022-11-29 19:43:32 +0530

    Create Solution.java

---
+++ Day58/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int chk=0;
+        int productOfArray = 1;
+        for(int i: nums){
+            if(i!=0)
+                productOfArray=i*productOfArray;
+            else
+                chk++;
+        }
+        for(int i=0; i<nums.length; i++){
+            if(chk==1){
+                if(nums[i] != 0)
+                    nums[i] = 0;
+                else
+                    nums[i]=productOfArray;
+            }else if(chk>1){
+                nums[i]=0;
+            }
+            else{
+                nums[i] = productOfArray/nums[i];
+            }
+        }
+        return nums;
+    }
+}
